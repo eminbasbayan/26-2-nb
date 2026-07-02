3f1c791e71650a95e3f9cbe517a56d862306a592
eminbasbayan <emin.basbayan@icloud.com>
2026-07-02 22:48:49 +0300

    json veri içeri aktarma ve watch modu

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const math = require('./add');
-const { cikar, topla } = require('./add');
+const data = require('./data.json');
 
-console.log(math.topla(5, 5));
-console.log(math.cikar(5, 5));
-console.log(cikar(5, 5));
-console.log(topla(5, 5));
+console.log('Benim JSON Datam: ', data);

f0e4a011381851162006a6282592bb1810003797
eminbasbayan <emin.basbayan@icloud.com>
2026-07-11 22:53:38 +0300

    http routing

--- server.js
+++ server.js
@@ -2,18 +2,30 @@ const http = require('node:http');
 const fs = require('node:fs');
 
 const server = http.createServer((request, response) => {
-  const egitmen = {
-    firstName: 'Emre',
-    lastName: 'Demir',
-  };
+  console.log(request.method);
 
-  response.writeHead(200, { 'content-type': 'text/html' });
-  let htmlFile = fs.readFileSync('./index.html', 'utf-8');
-  htmlFile = htmlFile.replace(
-    '{{fullName}}',
-    `${egitmen.firstName} ${egitmen.lastName}`,
-  );
-  response.end(htmlFile);
+  if (request.url === '/') {
+    const egitmen = {
+      firstName: 'Emre',
+      lastName: 'Demir',
+    };
+    response.writeHead(200, { 'content-type': 'text/html' });
+    let htmlFile = fs.readFileSync('./index.html', 'utf-8');
+    htmlFile = htmlFile.replace(
+      '{{fullName}}',
+      `${egitmen.firstName} ${egitmen.lastName}`,
+    );
+    response.end(htmlFile);
+  } else if (request.url === '/about') {
+    response.writeHead(200, { 'content-type': 'text/plain' });
+    response.end('About Page');
+  } else if (request.url === '/contact') {
+    response.writeHead(200, { 'content-type': 'text/plain' });
+    response.end('Contact Page');
+  } else {
+    response.writeHead(404);
+    response.end('Page not found!');
+  }
 });
 
 server.listen(3000, () => {

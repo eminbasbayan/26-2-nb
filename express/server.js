77babe55848380251d35489ad07a35ab0f85f0d4
eminbasbayan <emin.basbayan@icloud.com>
2026-07-25 22:24:16 +0300

    http get metot

--- express/server.js
+++ express/server.js
@@ -2,70 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const customers = [
-  {
-    id: 1,
-    firstName: 'Emin',
-    lastName: 'Başbayan',
-    role: 'user',
-  },
+const users = [
+  { id: 1, name: 'Ahmet', age: 25, email: 'ahmet@example.com' },
+  { id: 2, name: 'Ayşe', age: 30, email: 'ayse@example.com' },
 ];
 
-const user = {
-  id: 1,
-  firstName: 'Emin',
-  lastName: 'Başbayan',
-  role: 'user',
-};
-
-function isLogin(req, res, next) {
-  if (user) {
-    req.role = user.role;
-    req.adi = "Emin"
-    next();
-  } else {
-    res.status(401).send('Lütfen giriş yapın!');
-  }
-}
-
-function isAdmin(req, res, next) {
-  if (req.role === 'admin') {
-    next();
-  } else {
-    res.status(401).send('Kullanıcı yetkisi yok!');
-  }
-}
-
 app.get('/', (req, res) => {
-  /*   res.send("Hello World!") */
-  res.status(200).sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-
-app.get('/products-page', (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, 'views', 'products.html'));
-});
-
-app.get('/admin-dashboard', isLogin, isAdmin, (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, 'views', 'dashboard.html'));
-});
-
-app.get('/admin-customers', isAdmin, (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, 'views', 'customers.html'));
-});
-
-function sendCustomers(req, res) {
-  // db bağlantısı yapıp datayı çekebiliriz = customers
-
-  res.status(200).json(customers);
-}
-app.get('/api/customers', sendCustomers);
-
-app.get('/new-page', (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, 'views', 'new-page.html'));
-});
-
-app.get('/old-page', (req, res) => {
-  res.redirect(301, '/new-page');
+  res.status(200).json(users);
 });
 
 app.use((req, res) => {

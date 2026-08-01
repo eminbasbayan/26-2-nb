f6811e7730631386ef14d03245c16344179f2a9d
eminbasbayan <emin.basbayan@icloud.com>
2026-08-01 22:28:52 +0300

    login - register işlemleri

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,11 +5,16 @@ const {
   createUser,
   updateUser,
   deleteUser,
+  registerUser,
+  loginUser,
 } = require('../controllers/userController.js');
 
 router.get('/', getAllUsers);
 router.post('/', createUser);
 router.put('/', updateUser);
 router.delete('/:userId', deleteUser);
+
+router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 module.exports = router;

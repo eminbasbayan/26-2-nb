ded633ed706dcb922d0465c18f8545a9cb4871b9
eminbasbayan <emin.basbayan@icloud.com>
2026-08-06 21:56:12 +0300

    mongodb connection

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 const app = require('./app.js');
+const connectDB = require('./config/dbConfig.js');
 
 const PORT = 3000;
+
+connectDB()
 
 app.listen(PORT, () => {
   console.log(`Sunucu ${PORT} portunda çalışıyor!`);
